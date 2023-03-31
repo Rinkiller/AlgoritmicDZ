df1655757088da27057948581f353e1bb3a3c011
Rinkiller <Rinkiller@mail.ru>
2023-03-31 19:05:37 +0300

    nevcommit

---
+++ test.java
@@ -0,0 +1,19 @@
+class test{
+    public static void main(String[] args) {
+        
+        // сумма исел от 1... n
+        int n = 100000;
+        int result = 0;
+        for(int i = 1; i <= n; i++)
+            result += i;
+        System.out.println("result for CICLE:" + result);
+
+        System.out.println("next:");
+
+        System.out.println("result for function:"+ (n + n*n)/2 );
+
+
+        // нахождение простых чисел
+        for()
+    }
+}
